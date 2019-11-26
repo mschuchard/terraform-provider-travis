31068a0866d4dffb831803b33defc74bebeec580
Matt Schuchard <grapheneowns@gmail.com>
2019-11-26 15:56:11 -0500

    refactor travisopts

--- travis/client.go
+++ travis/client.go
@@ -10,16 +10,20 @@ import (
 type travisOpts struct {
   token      string
   commercial bool
+  method     string
+  endpoint   string
+  headers    map[string]string
+  body       []byte
 }
 
 // primary helper function for client connections to api endpoint
-func apiClient(method string, endpoint string, headers map[string]string, body []byte) (string, error) {
+func apiClient(opts *travisOpts) (string, error) {
   // construct url
   var url string
-  if &travisOpts.commercial {
-    url = "https://api.travis-ci.com/repo/" + endpoint
+  if opts.commercial {
+    url = "https://api.travis-ci.com/repo/" + opts.endpoint
   } else {
-    url = "https://api.travis-ci.org/repo/" + endpoint
+    url = "https://api.travis-ci.org/repo/" + opts.endpoint
   }
 
   // initialize http client
@@ -27,10 +31,10 @@ func apiClient(method string, endpoint string, headers map[string]string, body [
 
   // body into bytes.Buffer pointer
   var bodyBuffer *bytes.Buffer
-  bodyBuffer = bytes.NewBuffer(body)
+  bodyBuffer = bytes.NewBuffer(opts.body)
 
   // body always request body or nil for reads
-  request, err := http.NewRequest(method, url, bodyBuffer)
+  request, err := http.NewRequest(opts.method, url, bodyBuffer)
 
   // TODO: err handle
 
@@ -38,10 +42,10 @@ func apiClient(method string, endpoint string, headers map[string]string, body [
   request.Header.Add("Content-Type", "application/json")
   request.Header.Add("Accept", "application/json")
   request.Header.Add("Travis-API-Version", "3")
-  request.Header.Add("Authorization", &travisOpts.token)
+  request.Header.Add("Authorization", opts.token)
 
   // append extra headers; TODO: block conflict with above
-  for headerKey, headerValue := range headers {
+  for headerKey, headerValue := range opts.headers {
     request.Header.Add(headerKey, headerValue)
   }
 
