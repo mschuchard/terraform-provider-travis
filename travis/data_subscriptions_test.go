6307a73403ba886a8b7940242af920b1994e8c6c
Matt Schuchard <grapheneowns@gmail.com>
2019-12-30 16:23:27 -0500

    flesh out subscriptions data source test

--- travis/data_subscriptions_test.go
+++ travis/data_subscriptions_test.go
@@ -3,6 +3,28 @@ package travis
 import (
   "testing"
 
-  "github.com/hashicorp/terraform/helper/schema"
-  "github.com/hashicorp/terraform/terraform"
+  "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
+
+func TestDataSubscriptions(test *testing.T) {
+	resource.Test(test, resource.TestCase{
+		Providers: testProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSubscriptionsConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testDataSubscriptionsCheck("data.subscriptions.by_id", "subscriptions.test"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSubscriptionsCheck(name, reference string) resource.TestCheckFunc {
+	return resource.ComposeTestCheckFunc(
+		resource.TestCheckResourceAttrPair(name, "subscriptions", reference, "subscriptions"),
+	)
+}
+
+// this data source has no attribute arguments
+var testDataSubscriptionsConfig = `data "subscriptions" "test" {}`
